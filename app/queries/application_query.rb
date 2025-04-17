2d3e00dd6ccd6bd7af952c555c414976cf4859d4
tranquangvu <vutran.513@gmail.com>
2025-04-17 17:31:00 +0700

    Feat: improve query object

--- app/queries/application_query.rb
+++ app/queries/application_query.rb
@@ -1,30 +1,61 @@
-# Usage: read more at https://thoughtbot.com/blog/a-case-for-query-objects-in-rails
-
-module Scopeable
-  extend ActiveSupport::Concern
-
-  class_methods do
-    def scope(name, body)
-      define_method name do |*args, **kwargs|
-        relation = instance_exec(*args, **kwargs, &body)
-        relation || self
-      end
-    end
-  end
-end
-
+# Sample usage:
+#
+# module Products
+#   class GeneralQuery < ApplicationQuery
+#     def call(scope = Product.all, **params)
+#       scope = filter(scope, :by_term, params[:term])
+#       scope = filter(scope, :by_price, params[:price])
+#       scope = sort(scope, params[:sort])
+#       scope
+#     end
+#
+#     private
+#
+#     def by_term(scope, term)
+#       scope.where('name ILIKE ?', "%#{term}%")
+#     end
+#
+#     def by_price(scope, price)
+#       scope.where('price <= ?', price)
+#     end
+#   end
+# end
+#
+# Products::GeneralQuery.call(name: 'phone', price: 1000)
+# Products::GeneralQuery.call(name: 'phone', price: 1000, sort: 'price')
+# Products::GeneralQuery.call(name: 'phone', price: 1000, sort: '-price')
+# Products::GeneralQuery.call(Product.on_sale, name: 'phone', price: 1000)
+#
 class ApplicationQuery
-  attr_reader :scope
-
-  def self.call(scope = nil, ...)
-    new(scope).call(...)
+  def self.call(...)
+    new.call(...)
   end
 
-  def initialize(scope = nil)
-    @scope = scope
-  end
-
-  def call
+  def call(scope = nil, **)
     raise NotImplementedError, "You must define `call` as instance method in #{self.class.name} class."
+  end
+
+  private
+
+  def filter(scope, method, params)
+    return scope if params.blank?
+    send(method, scope, params)
+  end
+
+  def sort(scope, sorting)
+    return scope if sorting.blank?
+    column, direction = extract_sorting(sorting)
+    scope.order(column => direction)
+  end
+
+  def extract_sorting(value)
+    direction = :asc
+    column = value
+
+    if value.start_with?('+', '-')
+      direction = :desc if value.start_with?('-')
+      column = value[1..-1]
+    end
+    [column, direction]
   end
 end
